663c84405496a8a2c308eef5d4908a48c11bc37b
elvis <elvismtt@gmail.com>
2017-09-14 19:24:32 -0300

    One one method in mymod is enough

--- python_cpp/mymod/mymod.c
+++ python_cpp/mymod/mymod.c
@@ -20,23 +20,6 @@
  Functions
 ==----------------------------------------------------------==*/
 static PyObject *
-mymod_say_hello(PyObject *self, PyObject *args)
-{
-    const char *hello = "Hello World!";
-    PyObject *result = NULL;
-
-    if ((result = PyUnicode_FromString(hello)) == NULL) {
-        PyErr_SetString (
-            PyExc_TypeError,
-            "Could not build greeting string");
-        return NULL;
-    }
-
-    return result;
-}
-
-
-static PyObject *
 mymod_system(PyObject *self, PyObject *args)
 {
     const char *command = NULL;
@@ -65,18 +48,16 @@ mymod_system(PyObject *self, PyObject *args)
  Method table
 ==----------------------------------------------------------==*/
 static PyMethodDef mymod_methods[] = {
-    /* Members:
-     *   - name of the method
-     *   - pointer to the function
-     *   - calling convention
-     *   - docstring
-     */
     {
-        "say_hello", mymod_say_hello, METH_VARARGS,
-        "Says a greeting to the user"
-    },
-    {
-        "system", mymod_system, METH_VARARGS,
+        /* Members:
+         *   - name of the method
+         *   - pointer to the function
+         *   - calling convention
+         *   - docstring
+         */
+        "system",
+        mymod_system,
+        METH_VARARGS,
         "Execute a shell command"
     },
     
@@ -89,22 +70,17 @@ static PyMethodDef mymod_methods[] = {
  Module exported symbols
 ==----------------------------------------------------------==*/
 static struct PyModuleDef mymod_module = {
-    /* a misterious header */
+    /* Members:
+     *   - a misterious header
+     *   - name of module
+     *   - module doc string
+     *   - size of per-interpreter state of the module
+     *   - method defs
+     */
     PyModuleDef_HEAD_INIT,
-    
-    /* name of module */
     "mymod",
-    
-    /* module doc string */
     "Demo module for native extensions",
-    
-    /* size of per-interpreter state of the module,
-       or -1 if the module keeps state in
-       global variables. */
     -1,
-    
-    /* array of pointers to the functions that will
-       become the module's methods */
     mymod_methods   
 };
 
