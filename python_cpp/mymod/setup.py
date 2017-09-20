c51bfb5ae3f1e5188c4f4a7647bbe9c1668f4e84
Elvis Teixeira <elvismtt@gmail.com>
2017-09-20 12:53:04 -0300

    Update setup.py

--- python_cpp/mymod/setup.py
+++ python_cpp/mymod/setup.py
@@ -16,14 +16,14 @@
 
 from distutils.core import setup, Extension
 
-mymod = Extension(
-    'mymod',
-    sources = ['mymod.c']
-)
-
 setup(
     name = 'mymod',
     version = '1.0',
     description = 'This is a demo module',
-    ext_modules = [mymod]
+    ext_modules = [
+        Extension(
+            'mymod',
+            sources = ['mymod.c']
+        )
+    ]
 )
